e1671ff0ca0ef7e2b859a4ba9379aaaeaccc1bc1
swang <swang@c8r6s5.42seoul.kr>
2021-10-28 14:58:44 +0900

    update struct

--- phlio/include/philo.h
+++ phlio/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 17:16:18 by swang             #+#    #+#             */
-/*   Updated: 2021/10/26 18:59:55 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/28 14:57:09 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,28 @@
 
 typedef struct s_data
 {
-	int	num_of_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_of_must_eat;
-	pthread_mutex_t *fork;
-	pthread_mutex_t *print;
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_of_must_eat;
+	int				death_check;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	*print;
 }	t_data;
 
 typedef struct s_philo
 {
-	t_data *data;
-	pthread_mutex_t r_fork;
-	pthread_mutex_t l_fork;
+	t_data			*data;
+	int				eat_count;
+	int				my_death;
+	int				name;
+	unsigned long	last_eat;
+	pthread_t		thread;
+	pthread_t		monitor;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	eat;
 
 }	t_philo;
 
--- phlio/src/main.c
+++ phlio/src/main.c
@@ -6,27 +6,61 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 17:28:22 by swang             #+#    #+#             */
-/*   Updated: 2021/10/26 18:59:58 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/28 14:57:09 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+static int	check_arg(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (argc < 5 || argc > 6)
+		return (-1);
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (ft_isdigit(argv[i][j]) == 0)
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	malloc_struct(char *argv[], t_data *data, t_philo **philo)
+{
+	*philo = (t_philo *)malloc(sizeof((t_philo) * ft_atoi(argv[1]));
+
+
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc < 5 || argc > 6)
+	t_data	data;
+	t_philo	*philo;
+
+	if (check_arg(argc, argv) == -1)
 	{
-		pritnf("argument error. check your argument\n");
+		printf("Argument error. check your argument\n");
+		printf("Usage : ./philo [number_of_philosophers] [time_to_die] [time_to_eat] [time_to_sleep] [optional : number_of_times_each_philosopher_must_eat]\n");
 		return (-1);
 	}
-	else
+	if (malloc_struct(argv[1], &data, &philo) == -1)
 	{
-		t_philo *philo;
-		init_sturct(philo, argc, argv);
-		phliosopher(philo);
-		free(philo);
-		return (0);
+		printf("Failed to malloc.\n");
+		return (-1);
 	}
+	init_data(&data);
+	init_philo(&data, philo);
+	philosopher(&data, philo);
+	return (0);
 }
 
 /*
