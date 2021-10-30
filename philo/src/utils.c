20d0e66c308e82d1344ba6fee0f40ce05eb21b1f
swang <swang@c8r5s5.42seoul.kr>
2021-10-30 16:18:30 +0900

    add ft_free(), ft_atoi(), ft_isdigit()

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,51 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:28:32 by swang             #+#    #+#             */
-/*   Updated: 2021/10/28 16:28:35 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/30 14:53:03 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/philo.h"
+
+void	ft_free(t_data *data, t_philo *philo)
+{
+	//1. 동적할당한 애들 지워야함
+	if (philo == NULL)
+		free(philo);
+	if (data->fork == NULL)
+		free(data->fork);
+	if (data->death_check == NULL)
+		free(data->death_check);
+	exit(0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	num;
+
+	i = 0;
+	sign = 0;
+	num = 0;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			return (-1);
+		sign++;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+		num = (10 * num) + (str[i++] - '0');
+	if (num > 2147483647)
+		return (-1);
+	return (num);
+}
+
+int	ft_isdigit(int c)
+{
+	if ('0' <= c && c <= '9')
+		return (1);
+	else
+		return (0);
+}
