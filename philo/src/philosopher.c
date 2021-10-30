cbd3bf85d611cdfcdc5b413c165b27e50ccf5177
swang <swang@c8r5s5.42seoul.kr>
2021-10-30 16:17:49 +0900

    fix type error

--- philo/src/philosopher.c
+++ philo/src/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:03:02 by swang             #+#    #+#             */
-/*   Updated: 2021/10/28 16:32:22 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/30 15:42:06 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void	philosopher(t_data *data, t_philo *philo)
 	i = 0;
 	while (i < data->num_of_philo)
 	{
-		pthread_creat(&(philo[i].thread), NULL, &(philo_thread), (void *)philo[i]));
-		pthread_creat(&(philo[i].monitor), NULL, &(monitor_thread), (void *)philo[i]);
-		usleep(100); // 생성 후 시간줘서 쓰레드 꼬임 방지
+		pthread_create(&(philo[i].thread), NULL, &(philo_thread), &(philo[i]));
+		//pthread_create(&(philo[i].monitor), NULL, &(monitor_thread), &(philo[i]));
+		usleep(500); // 생성 후 시간줘서 쓰레드 꼬임 방지
 		i++;
 	}
 	i = 0;
 	while (i < data->num_of_philo)
 	{
-		pthread_join(&(philo[i].thread), 0);
-		pthread_join(&(philo[i].monitor), 0);
+		pthread_join(philo[i].thread, 0);
+		//pthread_join(philo[i].monitor, 0);
 		pthread_mutex_destroy(&(data->fork[i]));
-		pthread_mutex_destroy(&(philo[i]->eat));
+		pthread_mutex_destroy(&(philo[i].eat));
 		i++;
 	}
-	free_struct(data, filo);
+	ft_free(data, philo);
 }
