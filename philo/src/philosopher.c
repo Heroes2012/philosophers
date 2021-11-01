772109a907335870f6a9a3c84829c08981404618
swang <swang@MacBook-sh.local>
2021-11-01 15:01:22 +0900

    add init-last_eat, add print mutex destroy

--- philo/src/philosopher.c
+++ philo/src/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:03:02 by swang             #+#    #+#             */
-/*   Updated: 2021/10/31 18:00:36 by swang            ###   ########.fr       */
+/*   Updated: 2021/11/01 14:43:21 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	philosopher(t_data *data, t_philo *philo)
 	data->start_time = get_time();
 	while (i < data->num_of_philo)
 	{
+		philo[i].last_eat = data->start_time; // 시작시간을 여기서 넣어줌
 		pthread_create(&(philo[i].thread), NULL, &(philo_thread), &(philo[i]));
 		pthread_create(&(philo[i].monitor), NULL, &(monitor_thread), &(philo[i]));
 		usleep(10); // 생성 후 시간줘서 쓰레드 꼬임 방지
@@ -39,5 +40,6 @@ void	philosopher(t_data *data, t_philo *philo)
 		pthread_mutex_destroy(&(philo[i].eat));
 		i++;
 	}
+	pthread_mutex_destroy(data->print);
 	ft_free(data, philo);
 }
