25be84ac63dfa78f2a73aab61ef80e803bb961f1
swang <swang@c1r4s1.42seoul.kr>
2021-11-01 16:14:10 +0900

    update print part when drop fork

--- philo/src/print_mutex.c
+++ philo/src/print_mutex.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 17:05:36 by swang             #+#    #+#             */
-/*   Updated: 2021/11/01 15:33:31 by swang            ###   ########.fr       */
+/*   Updated: 2021/11/01 16:11:27 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,30 @@ void	print_mutex(int flag, t_philo *philo, char c)
 	else if (flag == FORK && *philo->data->death_check == 0)
 	{
 		if (c == 'l')
+		{
+			printf("%lums %d has taken a l_fork\n", get_time() - philo->data->start_time, philo->name);
 			printf(" philo: %d, grab l_fork : %p\n", philo->name, philo->l_fork);
+		}
 		else if (c == 'r')
+		{
+			printf("%lums %d has taken a r_fork\n", get_time() - philo->data->start_time, philo->name);
 			printf(" philo: %d, grab r_fork : %p\n", philo->name, philo->r_fork);
-		printf("%lums %d has taken a fork\n", get_time() - philo->data->start_time, philo->name);
+		}
 	}
 	else if (flag == THINK && *philo->data->death_check == 0)
 		printf("%lums %d is thinking\n", get_time() - philo->data->start_time, philo->name);
 	else if (flag == DROP && *philo->data->death_check == 0)
 	{
 		if (c == 'l')
+		{
+			printf("%lums %d drop the l_fork\n", get_time() - philo->data->start_time, philo->name);
 			printf(" philo: %d, drop l-fork : %p\n", philo->name, philo->l_fork);
+		}
 		else if (c == 'r')
+		{
+			printf("%lums %d drop the r_fork\n", get_time() - philo->data->start_time, philo->name);
 			printf(" philo: %d, drop r-fork : %p\n", philo->name, philo->r_fork);
+		}
 	}
 	usleep(10);//d약간의 딜레이
 	pthread_mutex_unlock(philo->data->print);
