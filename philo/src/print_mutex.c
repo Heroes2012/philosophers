395b31f49f6b550dbcc51555553d59b84d9aafec
swang <swang@MacBook-sh.local>
2021-11-01 15:01:52 +0900

    add coment

--- philo/src/print_mutex.c
+++ philo/src/print_mutex.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/31 17:05:36 by swang             #+#    #+#             */
-/*   Updated: 2021/10/31 17:57:19 by swang            ###   ########.fr       */
+/*   Updated: 2021/11/01 14:27:16 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ void	print_mutex(int flag, t_philo *philo)
 		printf("%lums %d has taken a fork\n", get_time() - philo->data->start_time, philo->name);
 	else if (flag == THINK && *philo->data->death_check == 0)
 		printf("%lums %d is thinking\n", get_time() - philo->data->start_time, philo->name);
-	usleep(10);
+	usleep(10);//d약간의 딜레이
 	pthread_mutex_unlock(philo->data->print);
 }
