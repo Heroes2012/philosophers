2b1cdb704b7df857be264d6f0984b8c329730a70
swang <swang@MacBook-sh.local>
2021-11-01 12:45:29 +0900

    Use print_mutex() instead of printf

--- philo/src/philo_thread.c
+++ philo/src/philo_thread.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 16:31:36 by swang             #+#    #+#             */
-/*   Updated: 2021/10/31 15:25:57 by swang            ###   ########.fr       */
+/*   Updated: 2021/10/31 18:08:12 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	*only_one(t_philo *philo)
 {
 	unsigned long	tmp_time;
 
-	printf("%lums %d has taken a fork\n", get_time() - philo->data->strat_time, philo->name);
+	print_mutex(FORK, philo);
 	tmp_time = get_time();
 	while (*philo->data->death_check == 0 && (get_time() - tmp_time < (unsigned long)philo->data->time_to_die))
-		usleep(500);
+		usleep(10);
 	return (0);
 }
 
@@ -27,13 +27,13 @@ int	grap_the_fork(t_philo *philo)
 {
 	if (pthread_mutex_lock(philo->l_fork) != 0)
 		return (-1);
-	printf("%lums %d has taken a fork\n", get_time() - philo->data->strat_time, philo->name);
+	print_mutex(FORK, philo);
 	if (pthread_mutex_lock(philo->r_fork) != 0)
 	{
 		pthread_mutex_unlock(philo->l_fork);
 		return (-1);
 	}
-	printf("%lums %d has taken a fork\n", get_time() - philo->data->strat_time, philo->name);
+	print_mutex(FORK, philo);
 	return (0);
 }
 
@@ -41,7 +41,6 @@ void	drop_the_fork(t_philo *philo)
 {
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
-	//printf("%d drop the fork\n", philo->name);
 }
 
 int	eat(t_philo *philo)
@@ -50,20 +49,16 @@ int	eat(t_philo *philo)
 	if (grap_the_fork(philo) == -1)
 		return (1); // 뮤택스 락 에러처리 1로 리턴하도록 변경
 	pthread_mutex_lock(&(philo->eat));
-	printf("%lums %d is eating\n", get_time() - philo->data->strat_time, philo->name);
+	print_mutex(EAT, philo);
 	philo->eat_count++; //time_to_eat : 먹는시간동안 철학자는 두개의 포크를 소지하고 있어야 한다.
 	philo->last_eat = get_time();
-	ft_usleep(philo->data->time_to_eat);
 	pthread_mutex_unlock(&(philo->eat));
-	pthread_mutex_lock(philo->data->print);
+	ft_usleep(philo->data->time_to_eat);
 	if (philo->eat_count == philo->data->num_of_must_eat)
 	{
 		drop_the_fork(philo);
-		printf("%d 다먹음\n", philo->name);
-		pthread_mutex_unlock(philo->data->print);
 		return (1);
 	}
-	pthread_mutex_unlock(philo->data->print);
 	drop_the_fork(philo);
 	return (0);
 }
@@ -77,18 +72,19 @@ void	*philo_thread(void *param)
 	if (philo->data->num_of_philo == 1)
 		return (only_one(philo));
 	if (philo->name % 2 == 0) //이름이 짝수인사람은 기다리는데 안기다려지는거 같음...
-		usleep(philo->data->time_to_eat);//얼마나 기다리지? -> 먹는 시간만큼
+		ft_usleep(philo->data->time_to_eat);//얼마나 기다리지? -> 먹는 시간만큼
 	while (*philo->data->death_check == 0)
 	{
 		if (eat(philo) == 1)
 			break ;
 		if (*philo->data->death_check == 1)
 			break ;
-		printf("%lums %d is sleeping\n", get_time() - philo->data->strat_time, philo->name);
+		print_mutex(SLEEP, philo);
 		ft_usleep(philo->data->time_to_sleep);
 		if (*philo->data->death_check == 1)
 			break ;
-		printf("%lums %d is thinking\n", get_time() - philo->data->strat_time, philo->name);
+		print_mutex(THINK, philo);
+		usleep(10);
 	}
 	//시간끌어오는 함수 확인하기....
 	return (0);
